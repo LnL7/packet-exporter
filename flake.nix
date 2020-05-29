da7ba8bd53d86c3a989b5fd5d52a25573bbd87ff
Daiderd Jordan <daiderd@gmail.com>
2020-05-29 18:49:09 +0200

    add flake and crate2nix expressions

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A flake for packet-exporter";
+
+  outputs = { self, nixpkgs }: {
+
+    crates.x86_64-darwin =
+      let pkgs = nixpkgs.legacyPackages.x86_64-darwin; in
+      import ./Cargo.nix {
+        inherit pkgs;
+        defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+          packet-exporter = attrs: {
+            buildInputs = [ pkgs.curl ]
+              ++ (with pkgs.darwin.apple_sdk.frameworks;
+                  nixpkgs.lib.optional pkgs.stdenv.isDarwin Security);
+          };
+        };
+      };
+
+    crates.x86_64-linux =
+      let pkgs = nixpkgs.legacyPackages.x86_64-linux; in
+      import ./Cargo.nix {
+        inherit pkgs;
+        defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+          packet-exporter = attrs: {
+            buildInputs = [ pkgs.curl ]
+              ++ (with pkgs.darwin.apple_sdk.frameworks;
+                  nixpkgs.lib.optional pkgs.stdenv.isDarwin Security);
+          };
+        };
+      };
+
+    defaultPackage.x86_64-darwin = self.crates.x86_64-darwin.rootCrate.build;
+    defaultPackage.x86_64-linux = self.crates.x86_64-linux.rootCrate.build;
+
+  };
+}
