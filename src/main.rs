65178183cad16938de8fdcfc59b5a2ee969e3f7f
Daiderd Jordan <daiderd@gmail.com>
2020-06-01 21:45:01 +0200

    make label values dynamic
    "maintenance" is another possible indicator, tracking the known labels
    ensures unexpected values are reset properly.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+use std::collections::HashSet;
 use std::env;
 use std::error::Error;
+use std::sync::{Arc, Mutex};
 
 use async_std::task;
 use lazy_static::lazy_static;
 use prometheus::{register_gauge_vec, Encoder, GaugeVec, TextEncoder};
 use serde_derive::{Deserialize, Serialize};
 use tide::http::StatusCode;
-use tide::{Body, Request, Response};
+use tide::{Body, Response};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct StatusPage {
@@ -30,20 +32,24 @@ lazy_static! {
     .unwrap();
 }
 
-async fn metrics(mut _req: Request<()>) -> Result<Response, tide::Error> {
-    for indicator in vec!["none", "minor", "critical"] {
-        PACKET_STATUS.with_label_values(&[indicator]).set(0 as f64);
-    }
-
+async fn metrics(labels: Arc<Mutex<HashSet<String>>>) -> Result<Response, tide::Error> {
     let page: StatusPage = surf::get(STATUS_URL).recv_json().await?;
     println!(
         "Packet status {}, {}",
         page.status.indicator, page.status.description
     );
 
+    let mut labels = labels.lock().unwrap();
+
+    for indicator in labels.iter() {
+        PACKET_STATUS.with_label_values(&[indicator]).set(0 as f64);
+    }
+
     PACKET_STATUS
         .with_label_values(&[&page.status.indicator])
         .set(1 as f64);
+
+    labels.insert(page.status.indicator);
 
     let encoder = TextEncoder::new();
     let metric_families = prometheus::gather();
@@ -55,9 +61,15 @@ async fn metrics(mut _req: Request<()>) -> Result<Response, tide::Error> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut server = tide::new();
+    let labels = Arc::new(Mutex::new(
+        vec!["none", "maintenance", "minor", "critical"]
+            .into_iter()
+            .map(String::from)
+            .collect(),
+    ));
 
-    server.at("/metrics").get(metrics);
+    let mut server = tide::new();
+    server.at("/metrics").get(move |_| metrics(labels.clone()));
 
     let port = env::var("PORT").unwrap_or(String::from("9122"));
     println!("Listening on http://127.0.0.1:{}", port);
